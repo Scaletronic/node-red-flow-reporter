32a6c83c3fcf7bf4546f8407ab67ab8a4126baf6
Andreas Markussen <andreas.markussen@gmail.com>
2024-02-07 08:34:55 +0100

    Added V2 for PrintNodeHeadline

---
+++ src/1_app/printNodeHeadline.spec.ts
@@ -0,0 +1,190 @@
+import { expect } from "chai"
+import { OutputWriter } from "../0_domain/OutputWriter.interface"
+import { NodesType } from "../0_domain/NodesType"
+import { nodesToTable, printNodeHeadline, setOutputWriter, tableToMarkdown } from "./printNodeHeadline"
+import exp from "constants"
+import { Table, Thead, Tr } from "../0_domain/Table.interface"
+
+let lines: string[] = []
+let fwOutput = ""
+const fakeFileWriter: OutputWriter = {
+  addOutputPart(outputPart) {
+    lines.push(outputPart)
+  },
+  writeOutputPart(reportFileName) {
+    fwOutput=lines.join("\n")
+//    return lines.join('\n')
+  },
+}
+
+const fw = fakeFileWriter
+
+const fileWriteLine = fw.addOutputPart
+setOutputWriter(fw)
+
+let nodes: NodesType = []
+let a = []
+
+function generateConfigObj(name:string) {
+  let config: any = []
+  config["name"] = name
+  config["otherInfo"] = 'other '+name
+  config["something head"] = "something"
+  return config
+}
+
+
+const parsedNRNode= {
+  id: '47e7261b12eaf02d',
+  z: 'be047adf.efd448',
+  type: 'dwsInject',
+  disabled: false,
+  config: {
+    name: 'S09 trigger cycle',
+    messageType: 'trigger/sensorCycle',
+    delayBetweenMessages: '',
+    howManyToWakeup: '',
+    pkgId: '',
+    stopReason: '',
+    sensorName: 'S09',
+    source: 'S09',
+    newPkgSource: 'Scan1',
+    newPkgBarcodeEnabled: true,
+    newPkgBarcode1: '123456',
+    newPkgBarcode2: '',
+    newPkgBarcode3: '',
+    newPkgBarcodeDelay: '',
+    newPkgBarcodeError: '',
+    newPkgDimensionsEnabled: true,
+    newPkgDimensionsDelay: '',
+    newPkgDimensionsWarning: '',
+    newPkgDimensionsError: '',
+    newPkgDimensionsHeight: '200',
+    newPkgDimensionsLength: '350',
+    newPkgDimensionsWidth: '200',
+    newPkgSortedEnabled: true,
+    newPkgSortedDelay: '',
+    newPkgSortedDropReason: '',
+    newPkgSortedLane: '1',
+    newPkgWeightEnabled: true,
+    newPkgWeight: '1500',
+    newPkgWeightDelay: '',
+    newPkgWeightWarning: '',
+    newPkgWeightError: '',
+    outputs: 1
+  },
+  parent: { // NR FLOW
+    id: 'be047adf.efd448',
+  },
+  //TYPE: Symbol(NRNode),
+  x: 210,
+  y: 460,
+  groupId: undefined,
+  showLabel: true,
+  inputLabels: [],
+  outputLabels: [],
+  wires: [ [Array] ],
+  outputCount: 1,
+  info: undefined,
+  inboundWires: [],
+  outboundWires: [ [] ] // NR WIRE
+}
+
+
+describe("Format a table ", () => {
+  it("Outer Test", () => {
+    const testNodes: NodesType = [
+      { type: "TestNode", id: '12345678', config: generateConfigObj("B1") },
+      { type: "TestNode", id:  "40868118", config: generateConfigObj("B2") }
+    ]
+    let nodes:any = {"TestNodes":testNodes}
+    //console.log("nodes", nodes)
+    const nodeNames = Object.keys(nodes).sort((a, b) => a.localeCompare(b));
+    nodeNames.forEach(nodeName => {
+      fileWriteLine(`## Node type '${nodeName}' is used ${nodes[nodeName].length} times`);
+      //console.log("nodes.nodename",nodes[nodeName])
+      printNodeHeadline(nodes[nodeName], nodes);
+      fileWriteLine("\n");
+    });
+    const output = fw.writeOutputPart("test")
+    // expect(output).to.eq("Bla")
+  })
+
+  it("printNodeHeadline - simple", () => {
+    lines = []
+    const testNodes: any = [
+      { type: "TestNode", name: "N1", config: generateConfigObj("B1") },
+      { type: "TestNode", name: "N2-40868118-123456", config: generateConfigObj("B2ABCDEFGHIJKLMN") }
+    ]
+    printNodeHeadline(testNodes, nodes)
+    fw.writeOutputPart("test")
+    const col1Rows = fwOutput.split("\n").map(row => row.split("|")[0])
+    const col1RowsLengths = col1Rows.map(cell => cell.length)
+    const shortestCellContent = Math.min(...col1RowsLengths)
+    const longestCellContent = Math.max(...col1RowsLengths)
+    //expect(shortestCellContent).to.eq(longestCellContent,`Lengths was ${col1RowsLengths.join(',')} `)
+    // expect(output).to.eq("bla")
+  })
+})
+
+describe("NodeRed Nodes to Table", () => {
+  const testNodes: any[] = [
+    { type: "TestNode", id:"123", name: "Node1", config:  generateConfigObj("Node1Con") },
+    { type: "TestNode", id:"234", name: "Node2", config: generateConfigObj("Node2Con") }
+  ]
+  // console.log("Test Nodes 1", testNodes[0])
+  it("should create a table", () => {
+    //TODO: DO NOT SORT the configs -
+    //REASON: The order of the columns is important, since adding a column should be posible
+    // ASK Jacob? Ask the team? 
+    const expectedTableHead: Thead = {
+      tds: [{ content: "name" }, {content: "id"}, {content: "otherInfo"} ,{ content: "something head" }]
+    }
+    const expectedTableBody: Tr[] = [
+      {
+        tds: [
+          { content: "Node1", headline: "name", parentRowName: "Node1" },
+          { content: "123", headline: "id", parentRowName: "Node1" },
+          { content: "other Node1Con", headline: "otherInfo", parentRowName: "Node1" },
+          { content: "something", headline: "something head", parentRowName: "Node1" }]
+      },
+      {
+        tds: [
+          { content: "Node2", headline: "name", parentRowName: "Node2" },
+          { content: "234", headline: "id", parentRowName: "Node2" },
+          { content: "other Node2Con", headline: "otherInfo", parentRowName: "Node2" },
+          { content: "something", headline: "something head", parentRowName: "Node2" }]
+      }
+    ]
+    console.log("Test Nodes 2", testNodes)
+    const expectedTable: Table = { nodeType: "TestNode" , head: expectedTableHead, trs: expectedTableBody }
+    const actualTable = nodesToTable(testNodes, testNodes)
+    const actualTableHead = actualTable.head
+    console.log("expectedTableHead", expectedTableHead)
+    console.log("actualTableHead", actualTableHead)
+    expect(actualTableHead).to.deep.eq(expectedTableHead)
+    expect(actualTable).to.deep.eq(expectedTable)
+  })
+})
+
+describe("Text to MD", () => {
+  const tableHead: Thead = {
+    tds: [{ content: "HA" }, { content: "HBBB" }, { content: "HCCC" }]
+  }
+  const tableBody: Tr[] = [
+    { tds: [{ content: "A" }, { content: "BB" }, { content: "CCC" }] },
+    { tds: [{ content: "AA" }, { content: "B" }, { content: "CCC" }] },
+    { tds: [{ content: "AAA" }, { content: "BBB" }, { content: "C" }] }
+  ]
+  const table: Table = { head: tableHead, trs: tableBody, nodeType: "TestNode"}
+  it("should create same space", () => {
+    const markdownTable = tableToMarkdown(table)
+    const expectedMarkdown = `| HA | HBBB | HCCC
+|----|------|-----
+|  A |   BB |  CCC
+| AA |    B |  CCC
+|AAA |  BBB |    C`
+    expect(markdownTable).to.eq(expectedMarkdown)
+  })
+})
+
--- src/1_app/printNodeHeadline.ts
+++ src/1_app/printNodeHeadline.ts
@@ -2,7 +2,9 @@ import { prettyFromCamelCase } from "../0_domain/units";
 import { extractConfigHeaders, extractHeaders } from "../0_domain/node-extractors";
 import { formatNodeInformation } from "../0_domain/formatters";
 import { maxLengths } from "../0_domain/maxLengths";
-import { OutputWriter } from "../0_domain/OutputWriter.interface";
+import { OutputSubPart, OutputWriter } from "../0_domain/OutputWriter.interface";
+import { NodesType } from "../0_domain/NodesType";
+import { Table, Td, Thead, Tr } from "../0_domain/Table.interface";
 
 let fwObj: OutputWriter | undefined = undefined; 
 
@@ -10,30 +12,173 @@ export function setOutputWriter(fw: OutputWriter) {
   fwObj = fw  
 }
 
-export function printNodeHeadline(nodes: any[]) {
+export function printNodeHeadline(nodes: any[], allNodes: NodesType) {
+  console.log("printNodeHeadline - old code - to be replaced when V2 is the standard")
   if (fwObj === undefined) throw new Error("Please set the output writer")
   const fileWriteLine = fwObj.addOutputPart
   const configHeadlines = extractConfigHeaders(nodes) as string[];
-  const nodeHeadlines = ['name', ...configHeadlines, ...extractHeaders(nodes) as string[]];
+  const nodeHeadlines = ['name', 'id', ...configHeadlines, ...extractHeaders(nodes) as string[]];
   fileWriteLine(nodeHeadlines.map(prettyFromCamelCase).join(" | "));
   fileWriteLine(nodeHeadlines.map(a => '-----').join("|"));
-  const sortedNodes = nodes.sort((a, b) => a.config['name'].localeCompare(b.config['name']));
+  const sortedNodes = nodes.sort((a, b) => a.config['name'] && a.config['name'].localeCompare(b.config['name']));
   sortedNodes.forEach(node => {
     const maxColLengthByHeadline = maxLengths(nodeHeadlines, sortedNodes) || 3;
-    const lineParts: string[] = [];
+    const cellContentForRow: string[] = [];
     nodeHeadlines.forEach(headline => {
       const maxColLength = maxColLengthByHeadline[headline] || 10;
       if (node[headline]) {
-        const b = "bla".padStart(10, '.');
-        let s: String = formatNodeInformation(nodes, node, headline).toString();
+        let s: String = formatNodeInformation(nodes, node, headline, allNodes).toString();
         s.padStart(10);
-        lineParts.push(formatNodeInformation(nodes, node, headline).toString().padStart(maxColLength));
+        cellContentForRow.push(formatNodeInformation(nodes, node, headline, allNodes).toString().padStart(maxColLength));
       } else if (node.config[headline]) {
-        lineParts.push(formatNodeInformation(nodes, node.config, headline).padStart(maxColLength));
+        cellContentForRow.push(formatNodeInformation(nodes, node.config, headline, allNodes).padStart(maxColLength));
       } else {
-        lineParts.push('&nbsp;');
+        cellContentForRow.push('&nbsp;');
       }
     });
-    fileWriteLine(lineParts.join(" | "));
+    fileWriteLine(cellContentForRow.join(" | ").trim());
   });
+}
+
+
+interface tableAndSubParts{
+  table: Table;
+  subParts: OutputSubPart[];
+}
+
+function TdContentToString(td: Td): Td {
+    const contentType = typeof td.content
+    let contentAsString = ""
+    switch (contentType) {
+      case "string": contentAsString =  td.content.toString(); break;
+      case "number": contentAsString =  td.content.toString(); break;
+      case "object": contentAsString =  JSON.stringify(td.content); break;
+      default: throw new Error(`Unknown type ${contentType}` + td.headline + td.parentRowName)
+    }
+  if (contentAsString.length == 0) contentAsString = '&nbsp;'
+    //throw new Error("Empty content:" + td.content + ":" + td.headline + td.parentRow.tds[0].content)  
+  return {...td, content: contentAsString }
+}
+
+export function guessFileTypeByContent(content: string): string {
+  if (content.startsWith('#')) return 'md'
+  if (content.startsWith('<')) return 'html'
+  if (content.startsWith('[') && content.endsWith(']')) return 'json'
+  if (content.startsWith('{') && content.endsWith('}')) return 'json'  
+  return 'txt'
+}
+
+export function tableFilterOutCropAndSaveToSubParts(table: Table): tableAndSubParts {
+  const subParts: OutputSubPart[] = []
+  const tableWithSubParts: tableAndSubParts = { table: table, subParts: [] }
+  table.trs.forEach(tr => tr.tds.forEach((td) => {
+    if (td.content == 'crop for the win' || td.content.length > 60) {
+      // Remove whitespace and replace with underscore
+      const fileName = table.nodeType +'-'+ td.headline + '_' + td.parentRowName.replace(/\s/g, '_') +'.'+ guessFileTypeByContent(td.content)
+      const subPart: OutputSubPart = { names: [fileName], content: td.content.toString()  }
+      subParts.push(subPart)
+      td.content = 'To long, moved to file' + fileName
+    }
+  }))
+  tableWithSubParts.subParts = subParts
+  return tableWithSubParts
+}
+
+export function printNodeHeadlineNewMarkdown(nodes: any[], allNodes: NodesType) {
+  if (fwObj === undefined) throw new Error("Please set the output writer")
+  const fileWriteLine = fwObj.addOutputPart
+  const nodeTable = nodesToTable(nodes, allNodes) 
+  const nodeTableWithStrings: Table = {
+    nodeType: nodes[0].type,
+    head: nodeTable.head, trs: nodeTable.trs.map(tr => ({ tds: tr.tds.map(TdContentToString) }))
+  };
+  const nodeTableWithSubParts = tableFilterOutCropAndSaveToSubParts(nodeTableWithStrings);
+  const tableMarkdown = tableToMarkdown(nodeTableWithSubParts.table)
+  fileWriteLine(tableMarkdown)
+  nodeTableWithSubParts.subParts.forEach(subPart => fwObj?.addSubPart(subPart.content, subPart.names))
+}
+
+/** @deprecated 
+ * this function is used in the old code, but it is not used in the new code
+*/
+export function arrayOfCellsToMarkdownRow(cells: string[]) {
+  console.log("deprecated function called")
+  const maxLength = Math.max(...cells.map(cell => cell.length))
+  const cellsPadded = cells.map(cell => cell.padStart(maxLength," "));
+  return cellsPadded.join(" | ")
+}
+
+
+export function nodeName(node: any): string {
+  return node.config.name || node.id 
+}
+
+
+export function nodesToTable(nodes: any[], allNodes: NodesType): Table {
+  const configHeadlines = extractConfigHeaders(nodes) as string[];
+  const nodeHeadlines = ['name', 'id', ...configHeadlines, ...extractHeaders(nodes) as string[]];
+  const tableHead: Thead = { tds: nodeHeadlines.map(headline => ({ content: headline })) }
+  /** Sort the nodes / rows in the table */
+  const sortedNodes = nodes.sort((a, b) => nodeName(a).localeCompare(nodeName(b)))
+  const tableBody: Tr[] = sortedNodes.map(node => {
+    const row: Tr = { tds: [] }
+    nodeHeadlines.forEach(headline => {
+      let td = { content: "", parentRowName: "", headline: "" }
+      let _content = ""
+      if (node[headline]) {
+        td.content = formatNodeInformation(nodes, node, headline, allNodes).toString()
+      } else if (node.config[headline]) {
+        td.content = formatNodeInformation(nodes, node.config, headline, allNodes).toString()
+      } else {
+        _content =  '&nbsp;' 
+      }
+      td.headline = headline
+      if (headline == 'name') td.parentRowName = td.content
+      else td.parentRowName = row.tds[0]?.content || row.tds[1]?.content
+      row.tds.push(td)
+    })
+    return row
+  })
+  return { nodeType: nodes[0].type, head: tableHead, trs: tableBody }
+}
+
+export function tableToMarkdown(table: Table): string {
+  const firstLength = Math.max(table.head.tds[0].content.length
+    , ...table.trs.map(tr => tr.tds[0].content.length))
+  const allLengthsByColumn = table.head.tds.map(td => td.content.length)
+  table.trs.forEach(tr => {
+    tr.tds.forEach((td, index) => {
+      allLengthsByColumn[index] = Math.max(allLengthsByColumn[index], td.content.length)
+    })
+  })
+  const tableHeadMarkdown = table.head.tds.map(td => td.content.padStart(firstLength)).join(" | ")
+  // replace all alphanumeric chars and whitespace with "-"
+  const tableLine = tableHeadMarkdown.replace(/[\w\s]/g, "-")
+  table.trs.forEach(tr => tr.tds.forEach((td) => {
+    if (!td.content) {
+      console.log("Empty content", tr.tds, td.content)
+      throw new Error("Empty content!!")
+    }   
+  }))
+  const tableBodyAsStrings = table.trs.map(tr => tr.tds.map((td, index) => {
+    const contentType = typeof td.content
+    let contentAsString = ""
+    switch (contentType) {
+      case "string": contentAsString = td.content.padStart(allLengthsByColumn[index]); break;
+      case "number": contentAsString =  td.content.toString().padStart(allLengthsByColumn[index]); break;
+      case "object": contentAsString =  JSON.stringify(td.content).padStart(allLengthsByColumn[index]); break;
+      default: throw new Error(`Unknown type ${contentType}`)
+    }
+    if(contentAsString.length == 0) throw new Error("Empty content")
+    if (contentAsString.length > 80)
+    {
+      console.log('cropping', contentAsString.slice(0,30))      
+      return 'crop for the win'
+
+      }
+      return contentAsString
+  }))
+  const tableBodyMarkdown = tableBodyAsStrings.map(row => '|'+row.join(" | ")).join("\n") 
+  const tableMarkdown = `|${tableHeadMarkdown}\n|${tableLine}\n${tableBodyMarkdown}`
+  return tableMarkdown
 }
