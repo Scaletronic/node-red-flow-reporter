a654989906be1df810d5a8ab2e79a8ba7265e9ea
Andreas Markussen <andreas.markussen@gmail.com>
2024-02-07 08:27:05 +0100

    Adding ID as a part of the irrelevant keys

--- src/0_domain/node-extractors.ts
+++ src/0_domain/node-extractors.ts
@@ -2,7 +2,7 @@ export function extractHeaders(similarNodes: any[]) {
   if (similarNodes === undefined || similarNodes.length === 0) return []
   const firstNode = similarNodes[0]
   const keys = Object.keys(firstNode)
-  const irrelevantKeys = ['x', 'y', 'z', 'type', 'TYPE', 'groupId', 'showLabel', 'wires', 'config', 'parent'] // we dont care about positions on the map
+  const irrelevantKeys = ['name', 'id', 'x', 'y', 'z', 'type', 'TYPE', 'groupId', 'showLabel', 'wires', 'config', 'parent'] // we dont care about positions on the map
   const relevantKeys = keys.filter(key => !irrelevantKeys.includes(key))
   return relevantKeys
 }
@@ -10,8 +10,9 @@ export function extractHeaders(similarNodes: any[]) {
 export function extractConfigHeaders(similarConfig: any[]) {
   if (similarConfig === undefined || similarConfig.length === 0) return []
   const firstNode = similarConfig[0]
+  if(firstNode === undefined || firstNode.config === undefined) return []
   const keys = Object.keys(firstNode.config)
-  const irrelevantKeys = ['name', 'x', 'y', 'z', 'type', 'TYPE', 'showLabel', 'wires', 'statusBroker'] // we dont care about positions on the map
+  const irrelevantKeys = ['name','id',  'x', 'y', 'z', 'type', 'TYPE', 'showLabel', 'wires', 'statusBroker'] // we don't care about positions on the map
   const relevantKeys = keys.filter(key => !irrelevantKeys.includes(key))
   return relevantKeys
 }
