f0ea62840053ed1ded830f3d18f3ffc5aab25e31
Andreas Markussen <am@scaletronic.dk>
2024-02-08 15:21:20 +0100

    1.0.6 - main changes

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,40 @@ const fs = require("fs");
 const { parseFlow } = require("@node-red/flow-parser");
 const FlowParser = require("@node-red/flow-parser");
 
+import * as packageJson from "./2_infra/version.info"
+
 import * as fw from "./2_infra/file-writer"
-import { printNodeHeadline, printNodeHeadlineNewMarkdown, setOutputWriter } from "./1_app/printNodeHeadline";
+import { printNodeHeadlineNewMarkdown, setOutputWriter } from "./1_app/printNodeHeadline";
 import { NodesType } from "./0_domain/NodesType";
 
 const fileWriteLine = fw.addOutputPart
 setOutputWriter(fw)
-
+console.log(packageJson.name, packageJson.version)
+console.log(` - ${packageJson.description}`)
 program
-  .name('flow-reporter')
+  .description(packageJson.name)
+  .name('node-red-flow-reporter')
+  .version(packageJson.version)
+  //.version(packageJson.version, '-v, --version', 'output the current version')
   .argument('<file>', 'input file name')
-  .option('-V', '--setVersion <type>', '1')
+  //.option('-V', '--setVersion <type>', '1')
   // .addOption(new Option('-f, --file <type>', 'input file name'))
   //.option('-f, --file <type>', 'input file name')
   .parse(process.argv);
 
-const options = program.opts();
 const args = program.args;
-console.log("args", args);
-console.log("options", options);
+// console.log("args", args);
 let fileName: string = "";
-let setVersion: string = "";
-if (options.V) {
-  if (options.V === true) setVersion = "2";
-  else setVersion = options.V.toString();
-  console.log(`Version set to \n\t${setVersion}`);
-}
+// let setVersion: string = "";
+// if (options.V) {
+//   if (options.V === true) setVersion = "2";
+//   else setVersion = options.V.toString();
+//   console.log(`Version set to \n\t${setVersion}`);
+// }
 if (args.length > 0) {
   fileName = args[0];
   console.log(`Filename given \n\t${fileName}`);
-  console.log("File size " + fs.statSync(fileName).size)
+  console.log("File size \n\t" + fs.statSync(fileName).size)
 }
 else{
   console.error("No filename given as argument");
@@ -76,20 +80,19 @@ flow.walk(function (obj: any) {
 
 
 function printNodes(nodes: NodesType): void {
-  console.log("setVersion", setVersion)
+ // console.log("setVersion", setVersion)
   const nodeNames = Object.keys(nodes).sort((a, b) => a.localeCompare(b))
   nodeNames.forEach(nodeName => {
     fileWriteLine(`## Node type '${nodeName}' is used ${nodes[nodeName].length} times`)
-    if(setVersion === "1"){
-      printNodeHeadline(nodes[nodeName], nodes)
-    } else if (setVersion === "2" ) {
+    // if(setVersion === "1"){
+    //   printNodeHeadline(nodes[nodeName], nodes)
+    // } else if (setVersion === "2" )
+    {
       printNodeHeadlineNewMarkdown(nodes[nodeName], nodes)
     }
-    else {
-      printNodeHeadline(nodes[nodeName], nodes)
-    }
-
-
+    // else {
+    //   printNodeHeadline(nodes[nodeName], nodes)
+    // }
     fileWriteLine("\n")
   })
 }
@@ -97,4 +100,4 @@ function printNodes(nodes: NodesType): void {
 printNodes(nodes)
 const reportFileName  =`${fileName}.md`
 fw.writeOutputPart(reportFileName)
-console.log(`Wrote ${reportFileName} to disk`)
+console.log(`Wrote \n\t${reportFileName} to disk`)
